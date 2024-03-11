ddd4f3efd823fd43d3e1472fd87615fc4eef006b
Lim Ming Jun <mingjun.lim@petronas.com>
2024-03-11 21:06:54 +0800

    Change CHOSEN_LLM to "Mistral" & EMBEDDMODEL to "text-embedding-3-small"

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config():
     # LLM Configs
     #=============================================================================================
     LLMCONFIG = dict(
-        # Options: Azure OpenAI (Default), Gemini
-        CHOSEN_LLM = "Azure OpenAI"
+        # Options: Azure OpenAI (Default), Gemini, Mistral
+        CHOSEN_LLM = "Mistral"
     )
     
     #=============================================================================================
@@ -12,8 +12,8 @@ class Config():
     #=============================================================================================
     EMBEDDINGCONFIG = dict(
         
-        # Options: text-embedding-ada-002 (Default), models/embedding-001
-        EMBEDDMODEL = "text-embedding-ada-002"
+        # Options: text-embedding-3-small (Default), models/embedding-001, mistral-embed
+        EMBEDDMODEL = "text-embedding-3-small"
     )
     
     #=============================================================================================
@@ -28,7 +28,7 @@ class Config():
         # score_threshold: Minimum relevance threshold for "similarity_score_threshold"
         # fetch_k: Amount of documents to pass to "mmr" algorithm (Default: 20). Usually, fetch_k parameter >> k parameter. This is because the fetch_k parameter is the number of documents that will be fetched before filtering.
         # lambda_mult: 1 for minimum diversity and 0 for maximum. (Default: 0.5)
-        SEARCH_KWARGS = {"k": 3, 
+        SEARCH_KWARGS = {"k": 4, 
             "fetch_k": 20, 
             "lambda_mult": 0.5,             
         },
