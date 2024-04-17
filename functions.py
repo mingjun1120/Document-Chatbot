b2defaafffbe4316e4fb5d2f610d9ea78bef0408
mingjun1120 <juroylim@gmail.com>
2024-04-17 23:27:35 +0800

    Use nomic-embed-text-v1 embedding model and ChromaDB. Use LCEL for conversation chat. Support docx and doc file type

--- functions.py
+++ functions.py
@@ -18,12 +18,8 @@ def get_document_text_chunks():
         loader = PyPDFLoader(os.path.join('temp_pdf_store', file)) # f"{os.getcwd()}\\temp_pdf_store\\{file}"
 
         # Get the text chunks of the PDF file, accumulate to the text_chunks list variable becaus load_and_split() returns a list of Document
-        docs_text_chunks += loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter(
-            chunk_size = 800,
-            chunk_overlap = 80,
-            length_function = len,
-            separators= ["\n\n", "\n", " ", ""]
-        ))
+        docs_text_chunks += loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100))
+        docs_text_chunks[0].page_content = docs_text_chunks[0].page_content.replace("Evaluation Warning: The document was created with Spire.Doc for Python.", "")
     return docs_text_chunks
 
 # Function to get embeddings
@@ -38,86 +34,77 @@ def get_vectors_embedding(docs_text_chunks):
     elif Config.LLMCONFIG.get('CHOSEN_LLM') == "Gemini":
         embeddings = GoogleGenerativeAIEmbeddings(model=Config.EMBEDDINGCONFIG.get('EMBEDDMODEL'), task_type="retrieval_query", google_api_key=st.secrets["GOOGLE_API_KEY"])
     elif Config.LLMCONFIG.get('CHOSEN_LLM') == "Mistral":
-        embeddings = AzureOpenAIEmbeddings(
-            deployment = Config.EMBEDDINGCONFIG.get('EMBEDDMODEL'),
-            openai_api_key = st.secrets["AZURE_OPENAI_API_KEY"], 
-            openai_api_version = "2024-02-01", 
-            azure_endpoint = st.secrets["AZURE_OPENAI_ENDPOINT"]
+        embeddings = NomicEmbeddings(
+            model=Config.EMBEDDINGCONFIG.get('EMBEDDMODEL'), 
+            nomic_api_key=st.secrets["NOMIC_API_KEY"]
         )
     else:
         raise ValueError("Please configure the embedding model in the config.py file!")
 
-    vector_embeddings = FAISS.from_documents(documents=docs_text_chunks, embedding=embeddings)
+    Chroma().delete_collection()
+    vector_embeddings = Chroma.from_documents(documents=docs_text_chunks, embedding=embeddings)
+    
     return vector_embeddings
 
 # Function to create the LLM chat agent
-def get_conversation_chain(vector_embeddings, memory):
+def get_conversation_chain(vector_embeddings):
 
     ############################################### CREATE LLM ###############################################
-    llm = create_llm()
+    llm_model = create_llm()
     
-    ############################################### CREATE PROMPTS ###############################################
-    # 1) Create a condense question prompt template for chat history
-    condense_question_template = """
-    You are a helpful and powerful AI Assistant for question-asnwering tasks. Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question in its original language.
+    ## Contextualize question 
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. DO NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm_model, 
+        vector_embeddings.as_retriever(
+            search_type = Config.VECTORSTORECONFIG.get("SEARCH_TYPE"),
+            search_kwargs = Config.VECTORSTORECONFIG.get("SEARCH_KWARGS"),
+        ), 
+        contextualize_q_prompt
+    )
     
-    Chat History: 
-    {chat_history}
-    Follow Up Input: {question}
-    Standalone question:
-    """
-    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_question_template)
+    # Create a QnA answering prompt template and the chain
+    qa_system_prompt = """You are an assistant for question-answering tasks. \
+    Use the following pieces of retrieved context to answer the question. \
+    If you don't know the answer, just say "I am sorry, I cannot find relevant info to answer this quesstion.". \
+    Try to keep the answer concise.\
 
-    # 2) Create a QnA answering prompt template
-    qa_template = """Answer the question based only on the following context:
-    {context}
-
-    Question: {question}
-    """
-    ANSWER_PROMPT = ChatPromptTemplate.from_template(qa_template)
-
-    # 3) Create a document prompt template
-    DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
-
-    ############################################### CREATE DOCUMENT FORMATTING METHOD ###############################################
-    def _combine_documents(docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"):
-        doc_strings = [format_document(doc, document_prompt) for doc in docs]
-        return document_separator.join(doc_strings)
+    {context}"""
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm=llm_model, prompt=qa_prompt)
     
-    # First we add a step to load memory. this adds a "memory" key to the input object
-    loaded_memory = RunnablePassthrough.assign(chat_history=RunnableLambda(memory.load_memory_variables) | itemgetter("history"))
-
-    # Now we calculate the standalone question
-    standalone_question = {
-        "standalone_question": {
-            "question": lambda x: x["question"],
-            "chat_history": lambda x: get_buffer_string(x["chat_history"]),
-        }
-        | CONDENSE_QUESTION_PROMPT
-        | llm
-        | StrOutputParser(),
-    }
-
-    # Now we retrieve the documents
-    retrieved_documents = {
-        "docs": itemgetter("standalone_question") | vector_embeddings.as_retriever(search_type = Config.VECTORSTORECONFIG.get("SEARCH_TYPE"), search_kwargs = Config.VECTORSTORECONFIG.get("SEARCH_KWARGS")),
-        "question": lambda x: x["standalone_question"],
-    }
-
-    # Now we construct the inputs for the final prompt
-    final_inputs = {
-        "context": lambda x: _combine_documents(x["docs"]),
-        "question": itemgetter("question"),
-    }
-
-    # And finally, we do the part that returns the answers
-    answer = {
-        "answer": final_inputs | ANSWER_PROMPT | llm,
-        "docs": itemgetter("docs"),
-    }
-
-    # And now we put it all together!
-    final_chain = loaded_memory | standalone_question | retrieved_documents | answer
+    # Create RAG Chain
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    
+    ### Statefully manage chat history ###
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in st.session_state.store_session_id:
+            st.session_state.store_session_id[session_id] = ChatMessageHistory()
+        return st.session_state.store_session_id[session_id]
+    
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
     
     # conversation_chain = ConversationalRetrievalChain.from_llm( # from_chain_type
     #     llm=llm, # Use the llm to generate the response, we can use better llm such as GPT-4 model from OpenAI to guarantee the quality of the response. For exp, the resopnse is more human-like
@@ -129,7 +116,7 @@ def get_conversation_chain(vector_embeddings, memory):
     #     condense_question_prompt=CUSTOM_QUESTION_PROMPT,
     #     condense_question_llm=llm # Can use cheaper and faster model for the simpler task like condensing the current question and the chat history into a standalone question with GPT-3.5 if you are on budget. Otherwise, use the same model as the llm
     # )
-    return final_chain
+    return conversational_rag_chain
 
 def create_llm():
     if Config.LLMCONFIG.get('CHOSEN_LLM') == "Azure OpenAI":
@@ -138,7 +125,7 @@ def create_llm():
             openai_api_key = st.secrets["AZURE_OPENAI_API_KEY"], 
             openai_api_version = "2024-02-01", 
             azure_endpoint = st.secrets["AZURE_OPENAI_ENDPOINT"],
-            max_tokens = 550,
+            max_tokens = None,
             model_kwargs={"top_p": 0.9}
         )
     elif Config.LLMCONFIG.get('CHOSEN_LLM') == "Gemini":
@@ -155,6 +142,23 @@ def save_uploadedfile(uploadedfile):
     with open(os.path.join("temp_pdf_store", uploadedfile.name), "wb") as f:
         f.write(uploadedfile.getbuffer())
 
+    if uploadedfile.name.endswith(".pdf") == False:
+        
+        # Get the file name without the extension
+        file_name = os.path.splitext(uploadedfile.name)[0] # Use -1 to get the file extension. E.g., '.docx'
+        
+        from spire.doc import Document, FileFormat
+
+        # Create a Document object
+        document = Document()
+
+        # Load a Word DOCX file
+        document.LoadFromFile(os.path.join("temp_pdf_store", uploadedfile.name))
+
+        # Save the file to a PDF file
+        document.SaveToFile(os.path.join("temp_pdf_store", f"{file_name}.pdf"), FileFormat.PDF)
+        document.Close()
+
 def initialize_session_state():
     # Set a default model
     if "gemini_pro_model" not in st.session_state:
@@ -166,17 +170,22 @@ def initialize_session_state():
         st.session_state.messages = []
     if "conversation_chain" not in st.session_state:
         st.session_state.conversation_chain = None
+    if "store_session_id" not in st.session_state:
+        st.session_state.store_session_id = {}
     if "docs" not in st.session_state:
         st.session_state.docs = None
     if "is_processed" not in st.session_state:
         st.session_state.is_processed = None
     if "is_vector_embeddings" not in st.session_state:
-        st.session_state.is_vector_embeddings = False
+        st.session_state.is_vector_embeddings = None
 
 def reset_session_state():    
     # Delete all the keys in session state
     for key in st.session_state.keys():
-        del st.session_state[key]
+        if key != "file_uploader_key":
+            del st.session_state[key]
+        else:
+            st.session_state["file_uploader_key"] += 1
     
     # Initialize the default session state variables again
     initialize_session_state()
@@ -188,6 +197,6 @@ def remove_files():
         # construct full file path
         # file = path + '\\' + file_name
         file = os.path.join(path, file_name)
-        if os.path.isfile(file) and file.endswith(".pdf"): # Only remove the PDF files
+        if os.path.isfile(file) and file.endswith(".bmp") == False: # Only remove files other than .bmp
             print('Deleting file:', file)
             os.remove(file)
